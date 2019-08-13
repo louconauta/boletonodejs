c378f0e9f4af34f49050685bd6208c1e99fcaf5d
Ricardo Fernando Ferreira <ricardo@iduka.com.br>
2019-08-13 16:44:18 -0300

    doing layout

--- index.js
+++ index.js
@@ -20,11 +20,71 @@ app.use(stylus.middleware(
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
+    var titlejson = {
+		logotipo:'sicredi.png',
+        textosuperior:'RECIBO DO PAGADOR',
+        localpagamento:'Local do Pagamento',
+        vencimento:'Vencimento',
+        beneficiario:'Beneficiário',
+        agenciacodigobeneficiario:'Agência/Código Beneficiário',
+        datadocumento:'Data do Documento',
+        numerodocumento:'Número do Documento',
+        especiedocumento:'Espécie do Documento',
+        aceite:'Aceite',
+        dataprocessamento:'Data de Processamento',
+        nossonumero:'Nosso Número',
+        usobanco:'Uso do Banco',
+        carteira:'Carteira',
+        especie:'Espécie',
+        quantidade:'Quantidade',
+        valor:'Valor',
+        valordocumento:'(=) Valor do Documento',
+        instrucoes:'Instruções (Todas as informações deste bloqueto são de exclusiva responsabilidade do beneficiário)',
+        descontoabatimento:'(-) Desconto/Abatimento',
+        outrasdeducoes:'(-) Outras deduções',
+        jurosmulta:'(+) Juros / Multa',
+        outrosacrescimos:'(+) Outros acréscimos',
+        valorcobrado:'(=) Valor Cobrado',
+        pagador:'Pagador',
+        sacadoravalista:'Sacador / Avalista',
+        codigobaixa:'Código de Baixa:'
+    }
+
+    var dadosjson = {
+        banco:'748',
+        bancodigito:'X',
+        localpagamento:'PAGÁVEL PREFERENCIALMENTE NAS COOPERATIVAS DE CRÉDITO DO SICREDI',
+        vencimento:'15/09/2019',
+        beneficiario:'NOME DO BENEFICIÁRIO',
+        agenciacodigobeneficiario:'3954.02.00092',
+        datadocumento:'06/05/2014',
+        numerodocumento:'123456/1',
+        especiedocumento:'DM',
+        aceite:'NÃO',
+        dataprocessamento:'06/05/2014',
+        nossonumero:'14/200001-0',
+        usobanco:'Teste',
+        carteira:'1',
+        especie:'REAL',
+        quantidade:'Teste',
+        valor:'Teste',
+        valordocumento:'100,00',
+        instrucoes:'',
+        descontoabatimento:'Teste',
+        outrasdeducoes:'Teste',
+        jurosmulta:'Teste',
+        outrosacrescimos:'Teste',
+        valorcobrado:'Teste',
+        pagador:'NOME DO PAGADOR - 000.000.000-01\nRUA FICTÍCIA\nPORTO ALEGRE RS 90000-000',
+        sacadoravalista:'Teste',
+        codigobaixa:'Teste',
+        linhadigitavel:'74891.11422 00001.039544 02000.921078 9 61870000010000'
+    }
 
     res.render('index',
     { 
-        title : 'Home',
-        barcode : "123"
+        title : titlejson,
+        data : dadosjson
     })
 })
 
